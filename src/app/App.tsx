a95f7aac2e5c00fcf989760aea8259a2cbcbe980
Stemmy287 <steem287@gmail.com>
2023-05-16 21:03:52 +0300

    feat: refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import 'app/App.scss';
+import React from 'react'
+import 'app/App.scss'
+import { Pages } from 'pages'
 
 export const App = () => {
-  return (
-    <div>
 
-    </div>
-  );
+	return <Pages />
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'index.css';
-import {App} from 'app/App';
-import reportWebVitals from 'reportWebVitals';
-import {Provider} from "react-redux";
-import {store} from "store";
-import {BrowserRouter} from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'index.css'
+import { App } from 'app/App'
+import reportWebVitals from 'reportWebVitals'
+import { Provider } from 'react-redux'
+import { store } from 'store'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
+	document.getElementById('root') as HTMLElement
+)
 root.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </BrowserRouter>
-);
+	<BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</BrowserRouter>
+)
 
-reportWebVitals();
+reportWebVitals()
