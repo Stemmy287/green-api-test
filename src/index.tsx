3655e693bc9ddc07f4f048d8999f9b7e495e8f63
Stemmy287 <steem287@gmail.com>
2023-05-16 13:45:50 +0300

    feat: files structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'index.css';
-import {App} from 'App';
+import {App} from 'app/App';
 import reportWebVitals from 'reportWebVitals';
+import {Provider} from "react-redux";
+import {store} from "store";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App/>
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>
 );
 
 reportWebVitals();
