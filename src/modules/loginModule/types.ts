1c4c0bdcd3f1fdff3a9a3c535e3d8249d0dfca87
Stemmy287 <steem287@gmail.com>
2023-05-17 14:16:40 +0300

    feat: response login type

--- src/modules/loginModule/types.ts
+++ src/modules/loginModule/types.ts
@@ -2,3 +2,7 @@ export type InstanceDataType = {
 	idInstance: string
 	apiTokenInstance: string
 }
+
+export type ResponseLoginType = {
+	stateInstance: 'notAuthorized' | 'authorized' | 'blocked' | 'sleepMode ' | 'starting'
+}
