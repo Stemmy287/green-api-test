c1718be7c0844786494d06945c016d534a685d3b
Stemmy287 <steem287@gmail.com>
2023-05-17 14:13:57 +0300

    feat: initialized flow

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'app/App.scss'
 import { Pages } from 'pages'
+import { useAppDispatch, useAppSelector } from 'hooks'
+import { isInitializedSelector, loginTC, setIsInitialized } from 'modules/loginModule'
 
 export const App = () => {
+
+	const dispatch = useAppDispatch()
+
+	const isInitialized = useAppSelector(isInitializedSelector)
+
+	useEffect(() => {
+		const instanceData = localStorage.getItem('instanceData')
+		if (instanceData) {
+			dispatch(loginTC(JSON.parse(instanceData)))
+		} else {
+			dispatch(setIsInitialized())
+		}
+	}, [dispatch])
+
+	if (!isInitialized) {
+		return <h1>...initialize</h1>
+	}
 
 	return <Pages />
 }
