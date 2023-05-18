87c66f561ce6ae43d64f75aefa1c53d513f12bd7
Stemmy287 <steem287@gmail.com>
2023-05-18 15:38:56 +0300

    feat: new items

--- src/modules/chatsModule/types.ts
+++ src/modules/chatsModule/types.ts
@@ -1,4 +1,26 @@
 export type ChatType = {
 	phoneNumber: number,
-	messages: string[]
+	messages: MessageType[]
+}
+
+export type MessageType = {
+	type: 'outgoing' | 'incoming'
+	timestamp: number
+	idMessage: string
+	statusMessage: 'pending' | 'sent' | 'delivered' | 'read'
+	typeMessage:
+		'textMessage'
+		| 'imageMessage '
+		| 'videoMessage'
+		| 'documentMessage'
+		| 'audioMessage'
+		| 'locationMessage'
+		| 'contactMessage'
+		| 'extendedTextMessage'
+	chatId: string
+	senderId: string
+	senderName: string
+	textMessage: string
+	downloadUrl: string
+	caption: string
 }
